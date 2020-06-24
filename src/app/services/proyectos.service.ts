107d5ccc946d1354b91b9cd87687ee3d543bc8e7
franco <francohuanaco@gmail.com>
2020-06-24 01:12:50 -0400

    Detalles portfolio items

--- src/app/services/proyectos.service.ts
+++ src/app/services/proyectos.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class ProyectosService {
 	private proyectos: Proyecto[] = [
 		{
-			titulo: 'Capacidades',
+			titulo: 'Capacidades.Org',
 			descripcion:
 				'Plataforma de educación online basada en el CMS Wordpress. Diseñada para el consumo masivo de cursos multimeda de diferente indole.',
 			proyecto: 'https://capacidades.org/',
@@ -15,7 +15,7 @@ export class ProyectosService {
 			posicion: true
 		},
 		{
-			titulo: 'spa-superhero-angular',
+			titulo: 'Superhero angular',
 			descripcion:
 				'App web desarrollada con Angular para la presentacion de diferentes heroes de DC commics y Marvel usando componentes, servicios y otras propiedades pertenecientes el Framework de Angular.',
 			proyecto: 'https://fhranko.github.io/spa-superhero-angular/',
@@ -24,7 +24,7 @@ export class ProyectosService {
 			posicion: false
 		},
 		{
-			titulo: 'spotiapp-angular',
+			titulo: 'Spotiapp angular',
 			descripcion:
 				'Proyecto desarrollado con Angular y Bootstrap usando la API de Spotify (haciendo el uso de pipes, servicios, componentes) para la presentación de nuevos lanzamientos, busqueda de artistas y su música.',
 			proyecto: '',
@@ -33,7 +33,7 @@ export class ProyectosService {
 			posicion: true
 		},
 		{
-			titulo: 'reserva-ambientes',
+			titulo: 'Gestión de ambientes',
 			descripcion:
 				'Sistema para gestionar los ambientes con los que se cuenta en las oficinas de la AGETIC en el marco del programa de voluntariado del área de investigación. Desarrollado principalmente con el framework Laravel.',
 			proyecto: '',
@@ -42,7 +42,7 @@ export class ProyectosService {
 			posicion: false
 		},
 		{
-			titulo: 'chicas-waskiris',
+			titulo: 'Proyecto Waskiri',
 			descripcion:
 				'Diseñado para ser un proyecto final y cimentar los conocimientos del taller de “HTML y Css” taller desarrollado por mi persona, en el marco del proyecto de "Chicas Waskiris" de la AGETIC.',
 			proyecto: 'https://fhranko.github.io/proyecto-chicas-waskiris/',
